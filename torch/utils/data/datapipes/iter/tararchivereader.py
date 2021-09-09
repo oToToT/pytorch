91288a3192439b9c68b732cea4d027e73efbe849
Kevin Tse <ktse@fb.com>
2021-09-09 18:24:37 -0400

    [DataPipe] Remove TarArchiveReader's dependency on FileLoader

--- torch/utils/data/datapipes/iter/tararchivereader.py
+++ torch/utils/data/datapipes/iter/tararchivereader.py
@@ -1,6 +1,5 @@
 from torch.utils.data import IterDataPipe
-from torch.utils.data.datapipes.utils.common import validate_pathname_binary_tuple
-from typing import Iterable, Iterator, Tuple, Optional, IO, cast
+from typing import Iterable, Iterator, Tuple
 from io import BufferedIOBase
 
 import os
@@ -10,11 +9,11 @@ import warnings
 class TarArchiveReaderIterDataPipe(IterDataPipe[Tuple[str, BufferedIOBase]]):
     r""" :class:`TarArchiveReaderIterDataPipe`.
 
-    Iterable datapipe to extract tar binary streams from input iterable which contains tuples of
-    pathname and tar binary stream, yields pathname and extracted binary stream in a tuple.
+    Iterable datapipe to extract tar binary streams from input iterable which contains pathnames,
+    yields a tuple of pathname and extracted binary stream.
 
     Args:
-        datapipe: Iterable datapipe that provides pathname and tar binary stream in tuples
+        datapipe: Iterable datapipe that provides pathnames
         mode: File mode used by `tarfile.open` to read file object.
             Mode has to be a string of the form 'filemode[:compression]'
         length: a nominal length of the datapipe
@@ -26,24 +25,21 @@ class TarArchiveReaderIterDataPipe(IterDataPipe[Tuple[str, BufferedIOBase]]):
     """
     def __init__(
         self,
-        datapipe: Iterable[Tuple[str, BufferedIOBase]],
+        datapipe: Iterable[str],
         mode: str = "r:*",
         length: int = -1
     ):
         super().__init__()
-        self.datapipe: Iterable[Tuple[str, BufferedIOBase]] = datapipe
+        self.datapipe: Iterable[str] = datapipe
         self.mode: str = mode
         self.length: int = length
 
     def __iter__(self) -> Iterator[Tuple[str, BufferedIOBase]]:
-        if not isinstance(self.datapipe, Iterable):
-            raise TypeError("datapipe must be Iterable type but got {}".format(type(self.datapipe)))
-        for data in self.datapipe:
-            validate_pathname_binary_tuple(data)
-            pathname, data_stream = data
+        for pathname in self.datapipe:
+            if not isinstance(pathname, str):
+                raise TypeError(f"pathname should be of string type, but is type {type(pathname)}")
             try:
-                # typing.cast is used here to silence mypy's type checker
-                tar = tarfile.open(fileobj=cast(Optional[IO[bytes]], data_stream), mode=self.mode)
+                tar = tarfile.open(name=pathname, mode=self.mode)
                 for tarinfo in tar:
                     if not tarinfo.isfile():
                         continue
@@ -52,11 +48,7 @@ class TarArchiveReaderIterDataPipe(IterDataPipe[Tuple[str, BufferedIOBase]]):
                         warnings.warn("failed to extract file {} from source tarfile {}".format(tarinfo.name, pathname))
                         raise tarfile.ExtractError
                     inner_pathname = os.path.normpath(os.path.join(pathname, tarinfo.name))
-                    # Add a reference of the source tarfile into extracted_fobj, so the source
-                    # tarfile handle won't be released until all the extracted file objs are destroyed.
-                    extracted_fobj.source_ref = tar  # type: ignore[attr-defined]
-                    # typing.cast is used here to silence mypy's type checker
-                    yield (inner_pathname, cast(BufferedIOBase, extracted_fobj))
+                    yield (inner_pathname, extracted_fobj)  # type: ignore[misc]
             except Exception as e:
                 warnings.warn(
                     "Unable to extract files from corrupted tarfile stream {} due to: {}, abort!".format(pathname, e))
--- torch/utils/data/datapipes/utils/common.py
+++ torch/utils/data/datapipes/utils/common.py
@@ -2,7 +2,6 @@ import os
 import fnmatch
 import warnings
 
-from io import BufferedIOBase
 from typing import Iterable, List, Union
 
 
@@ -53,14 +52,3 @@ def get_file_binaries_from_pathnames(pathnames: Iterable, mode: str):
             raise TypeError("Expected string type for pathname, but got {}"
                             .format(type(pathname)))
         yield (pathname, open(pathname, mode))
-
-
-def validate_pathname_binary_tuple(data):
-    if not isinstance(data, tuple):
-        raise TypeError("pathname binary data should be tuple type, but got {}".format(type(data)))
-    if len(data) != 2:
-        raise TypeError("pathname binary tuple length should be 2, but got {}".format(str(len(data))))
-    if not isinstance(data[0], str):
-        raise TypeError("pathname binary tuple should have string type pathname, but got {}".format(type(data[0])))
-    if not isinstance(data[1], BufferedIOBase):
-        raise TypeError("pathname binary tuple should have BufferedIOBase based binary type, but got {}".format(type(data[1])))
